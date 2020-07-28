f8107d734dcb7e44baaad880f779f5862b6d069b
OMCH <omch20012@gmail.com>
2020-07-28 19:45:28 -0400

    Started work on title screen

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Utilities : MonoBehaviour
+{
+    public static void typeText(string givenTag)
+    {
+        var textObject = GameObject.FindWithTag(givenTag).GetComponent<TextMeshProUGUI>();
+        var interiorText = textObject.text;
+
+        textObject.text = "";
+
+        for (int i = 0; i < interiorText.Length; i++)
+        {
+            interiorText += interiorText[i];
+            //waitSeconds(1);
+        }
+    }
+
+    public static IEnumerator waitSeconds(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+}
