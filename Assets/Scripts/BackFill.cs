86688df6701b3654d6a8f8255629aa10d9c33e25
OMCH <omch20012@gmail.com>
2020-08-01 10:13:46 -0400

    Working on more title stuff

--- Assets/Scripts/BackFill.cs
+++ Assets/Scripts/BackFill.cs
@@ -7,22 +7,39 @@ public class BackFill : MonoBehaviour
 {
     public float typeSpeed;
     public int charLength;
-
-    private TextMeshProUGUI textObject;
+    public GameObject fillPrefab;
 
     void Start()
     {
         //Utilities.typeText("backfill");
 
-        textObject = GetComponent<TextMeshProUGUI>();
         StartCoroutine(writeWait(typeSpeed));
     }
 
     IEnumerator writeWait(float speed)
     {
+        float xPlacement = -8.5f;
+        float yPlacement = 4.5f;
+
         for (int i = 0; i < charLength; i++)
         {
-            textObject.text += getRandomCharacter("alphaNum");
+            var newFill = Instantiate(fillPrefab);
+            newFill.transform.SetParent(gameObject.transform);
+
+            newFill.transform.position = new Vector2(xPlacement, yPlacement);
+            xPlacement += 0.5f;
+
+            ArrayList array = new string[] { 34, 69, 104, 139, 174, 209, 244, 279, 314, 349, 384, 419, 454 };
+
+            if (Utilities.doesContain(i, array) == true)
+            {
+                xPlacement = -8.5f;
+                yPlacement -= 0.75f;
+            }
+
+            var fillTMP = newFill.GetComponent<TextMeshProUGUI>();
+            fillTMP.text += getRandomCharacter("alphaNum");
+
             yield return new WaitForSeconds(speed);
         }
     }
@@ -34,10 +51,10 @@ public class BackFill : MonoBehaviour
         switch (givenType)
         {
             case "alpha":
-                characters = "abcdefghijklmnopqrstuvwxyz";
+                characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
                 break;
             case "alphaNum":
-                characters = "aabbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz0123456789";
+                characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
                 break;
             case "num":
                 characters = "0123456789";
--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -26,4 +26,24 @@ public class Utilities : MonoBehaviour
             //interiorText += interiorText[i];
         }
     }
+
+    public static bool doesContain(int givenInt, ArrayList givenArray)
+    {
+        var checker = false;
+
+        for (int i = 0; i < givenArray.Count; i++)
+        {
+            if (givenArray[i] == givenInt)
+            {
+                checker = true;
+            }
+        }
+
+        if (checker != true)
+        {
+            checker = false;
+        }
+
+        return checker;
+    }
 }
