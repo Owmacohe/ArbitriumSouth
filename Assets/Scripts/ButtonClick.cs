24ae8ea6301fa77660bd66ce2f82d6483e152577
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2020-08-05 18:59:46 -0400

    Added prologue

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        switch (gameObject.tag)
+        {
+            case "Start":
+                SceneManager.LoadScene("Prologue");
+                break;
+            case "Exit":
+                Application.Quit();
+                break;
+            case "Settings":
+                SceneManager.LoadScene("Settings");
+                break;
+        }
+    }
+}
